4c85f63cedfe7c729c9e05456ac0bf152aec499c
nZeep <77229906+nZeep@users.noreply.github.com>
2021-01-10 12:05:06 +0100

    Update Q_LinkedList - __str__.py

--- LINFO1101/[INFO1] Informatique 1 - Introduction à la programmation/Session 11/Q_LinkedList - __str__.py
+++ LINFO1101/[INFO1] Informatique 1 - Introduction à la programmation/Session 11/Q_LinkedList - __str__.py
@@ -1,13 +1,7 @@
-def __str__(self):
-    parse_list = []
-    element = self.__head
-    if element is not None:
-        parse_list.append(element.value())
-        while element.next() is not None:
-            element = element.next()
-            parse_list.append(element.value())
-    string_list = "["
-    for i in parse_list:
-        string_list += " {}".format(i)
-    string_list += " ]"
-    return string_list
+def __str__(self) :
+    string_list = "[ "
+    element = self.first()
+    while element is not None :
+        string_list += "{} ".format(element.value())
+        element = element.next()
+    return string_list + "]"
